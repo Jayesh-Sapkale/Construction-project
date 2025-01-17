b0c4f13303d1479bc586a49e1beb36a77c627f2c
Jayesh-Sapkale <jayeshsapkale792@gmail.com>
2025-01-17 22:55:57 +0530

    first commit

---
+++ construction/src/main/java/com/constuction/ConstructionApplication.java
@@ -0,0 +1,13 @@
+package com.constuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionApplication.class, args);
+	}
+
+}
